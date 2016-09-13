e3a62fa5cc9bd00c9d1216ab67a59d689d54489e
info <info@sciits.com>
2016-09-13 17:57:04 +0800

    Provided a special view for user's joined quests

--- common/models/usersquests.js
+++ common/models/usersquests.js
@@ -1,3 +1,36 @@
 module.exports = function(Usersquests) {
 
+    Usersquests.view = function(userId, cb) {
+		var Quests = Usersquests.app.models.Quests;
+		Usersquests.find(function(err, arr) {
+            if (err) return cb(err);
+			Quests.find(function(err, data) {
+				// console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Usersquests.all userId [' + userId + '] logged in! data = ' + data + " err = " + err);
+	            // cb(null, data);
+	            var arr2 = [];
+	            for(var i = 0; i < arr.length; i++) {
+		            for(var j = 0; j < data.length; j++) {
+		            	if(arr[i].usersid == userId && arr[i].questsid == data[j].id) {
+		            		// console.log(data[j]);
+		            		arr2.push(data[j]);
+		            	}
+		            }
+	            }
+	            cb(null, arr2);
+
+			});
+
+            // console.log(arr);
+            // cb(null, arr);
+    	});
+	};
+
+    Usersquests.remoteMethod(
+        'view',
+        {
+          http: {path:'/view', verb:'get'},
+          accepts: [{arg: 'userId', type: 'string'}],
+          returns: {arg: 'quests', type: 'string'}
+        }
+    );
 };
