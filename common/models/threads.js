f5801d79416eb5ef62c5a9728f4ba8c29b13620e
info <info@sciits.com>
2016-10-05 21:00:02 +0800

    Chaned the walls to be ordered like Drupal's Article - sorted latest topics on the top but leave the comments as they are

--- common/models/threads.js
+++ common/models/threads.js
@@ -23,28 +23,6 @@ function predicatBy(prop, desc) {
 		return 0;
 	}
 }
-// function getFlagByMessageIdAndOwner(data, msgId, ownerId) {
-//     var ret;
-//     // debugger
-//     console.log("13 =====================> component.getFlagByMessageId: searching in data [");
-//     // console.log(data);
-//     console.log(']  by msgId [' + msgId + '] ownerId [' + ownerId + ']');
-//     var f1, f2;
-//     for(var i=0; i<data.length; i++) {  //TODO why is data.length is null here????
-//         if(data[i].messages == msgId) f1 = true;
-//         if(data[i].subject.indexOf(ownerId) > -1) f2 = true;  //TODO this has to be owner's id!
-//         // debugger
-//         if(f1 && f2) {
-//             ret = data[i];
-//             console.log("component.getFlagByMessageIdAndOwner() found: ret.messages [" + ret.messages + ']');
-//             break;
-//         }
-//         f1 = f2 = false;
-//     }
-//     console.log("component.getFlagByMessageIdAndOwner(): ret [" + ret + ']');
-//     return ret;
-// }
-
 //TODO the following needs to be in a common place!!!
 
 module.exports = function(Threads) {
@@ -52,15 +30,6 @@ module.exports = function(Threads) {
     Threads.saveLike = function(messageId, ownerId, ownerName, cb) {
         var Flags = Threads.app.models.Flags;
         console.log(`threads.js saveLike() entered ${messageId} ${ownerId} ${ownerName}`);
-        //TODO the owner type need to be updated!
-        // var messageId = that.item.id;
-        // var ownerId;
-        // try {
-        //     ownerId = localStorage.getItem(stemcfg.userid); //data[0].id;
-        // } catch(e) {
-            // console.log(data);
-        //     throw e;
-        // }
         var items = [];
         var json3 = {
             data: {
@@ -122,38 +91,14 @@ module.exports = function(Threads) {
                     }
                     console.log(`threads.js new json4.data.likecount: ${json4.data.likecount}`);
                     function updateItem(data) {
-                        // var scope = StemFactory.get('wallCtrl');
-                        // scope.$apply(function () {
-                            //copy all values into data and update only the like count
-                            // if(typeof data !== 'undefined') {
-                                // var temp = data.likecount;
-                                // data = items[itemIndex];
-                                // data.likecount = temp;
-                                // if(currentFlag) {
-                                //     data.loggedinuserliked = 1;
-                                // } else {
-                                //     data.loggedinuserliked = 0;
-                                // }
-                                // items.splice(itemIndex, 1, data);  //support fetch
-                                cb(null, data);
-                            // } else {
-                            //     cb(null, {});
-                            // }
-                            // $ionicLoading.hide();
-                        // });
+                        cb(null, data);
                     }
-
-                    // capi(webHost, '/api/Messages', 'PUT', 'model', 'method', json4, updateItem, null);
                     setMessage(json4.data, updateItem);
                 }
-                // capi(webHost, `/api/Messages/${messageId}`, 'GET', 'model', 'method', {"id": messageId}, updateMessageLikeCount, null);
                 getMessage(messageId, updateMessageLikeCount);
             }
-            // capi(webHost, '/api/Flags', 'PUT', 'model', 'method', json3, updateLikeUI, null);
             setMessageFlag(json3.data, updateLikeUI);
         } //updateFlags end
-        // capi(webHost, '/api/Flags', 'GET', 'model', 'method', json3, updateUserFlags, null, 'swagger');
-        // capi(webHost, '/api/Flags', 'GET', 'model', 'method', json3, updateUserFlags, null);
         getMessageFlag(messageId, ownerId, updateUserFlags);
     }; //saveIt end
 
@@ -234,8 +179,10 @@ module.exports = function(Threads) {
     //=== http://apidocs.strongloop.com/loopback/#persistedmodel
     Threads.wall = function(loggedinuser, cb) {
       var sortedMessages = {};
-var data3 = [];
+      var data3 = [];
       Threads.find(function (err, data) {
+          data.reverse(); //new topic on the top but leave the comment(s) as is
+
 		  //TODO the following codes are not optimized!!!
 		  var Messages = Threads.app.models.Messages;
 	      Messages.find(function (err, data1) {
@@ -251,62 +198,39 @@ var data3 = [];
 						//added thread id
 						item1.threadid = item.id;
 						item1.ownerName = getOwnerName(item1.subject);
-						// getMessageFlag(item1.id, loggedinuser, function(flag) {
-						// 	item1.loggedinuserliked = flag;
-						// 	console.log('threads.js isMessageLiked message [');
-						// 	console.log(item1);
-						// 	console.log(']');
-							// data3.push(item1);	//does not work!!!
-						// });
-
-						// console.log('threads.js isMessageLiked message [');
-						// console.log(item1);
-						// console.log(']');
 						data3.push(item1);	//create a thread for each message ---- THIS happens before ****** 1 ****** !!!!!!
-                        // data3.reverse(); //TODO this is a start but it shouldn't messed up the comments!
 					}
 				}
 			}
-		    //=== sort it based on owner's, message type and timestamp
-			// sortedMessages = data3.sort(predicatBy('owner'));
-			// sortedMessages = sortedMessages.sort(predicatBy('type'));
-			// sortedMessages = sortedMessages.sort(predicatBy('createdat', true));
-			// sortedMessages = data3.sort(function(obj1, obj2) {
-			// 	if(!obj2.type.indexOf('-owner') > -1)	{
-			// 		return obj1.id - obj2.id;  //asc for comment
-			// 	} else {
-			// 		return obj2.id - obj1.id;
-			// 	}
-			// });
 
-//TODO not optimized - second loop is required due to the issue of calling APIs in three level deep!!!
-var data4 = [];
-var Flags = Threads.app.models.Flags;
-Flags.find(function (err, flags) {
-	var itemIndex = 0;
-	for (let item3 of data3) {
-		//added extra flag for UI
-		for (let item4 of flags) {
-			item3.loggedinuserliked = -1;
-			// console.log(`threads.js comparing message item4.owner=${item4.owner} with loggedinuser=${loggedinuser} and item4.messages=${item4.messages} with item3.id=${item3.id}`);
-			if(item4.owner == loggedinuser && item4.messages == item3.id) {
-				item3.loggedinuserliked = item4.state;
-				// item3.loggedinuserliked = 1;
-				// console.log(`message item3.loggedinuserliked set to ${item3.loggedinuserliked} itemIndex = ${itemIndex} item4.state = ${item4.state}`);
-				break;
-			}
-			// console.log('threads.js isMessageLiked message [');
-			// console.log(item3);
-			// console.log(']');
-            data3.splice(itemIndex, 1, item3);
+            //TODO not optimized - second loop is required due to the issue of calling APIs in three level deep!!!
+            var data4 = [];
+            var Flags = Threads.app.models.Flags;
+            Flags.find(function (err, flags) {
+            	var itemIndex = 0;
+            	for (let item3 of data3) {
+            		//added extra flag for UI
+            		for (let item4 of flags) {
+            			item3.loggedinuserliked = -1;
+            			// console.log(`threads.js comparing message item4.owner=${item4.owner} with loggedinuser=${loggedinuser} and item4.messages=${item4.messages} with item3.id=${item3.id}`);
+            			if(item4.owner == loggedinuser && item4.messages == item3.id) {
+            				item3.loggedinuserliked = item4.state;
+            				// item3.loggedinuserliked = 1;
+            				// console.log(`message item3.loggedinuserliked set to ${item3.loggedinuserliked} itemIndex = ${itemIndex} item4.state = ${item4.state}`);
+            				break;
+            			}
+            			// console.log('threads.js isMessageLiked message [');
+            			// console.log(item3);
+            			// console.log(']');
+                        data3.splice(itemIndex, 1, item3);
 
-		}
-		itemIndex++;
-	}
-	sortedMessages = data3;
-	// sortedMessages = data4;
-	cb(null, sortedMessages);	
-});	//Flags.find end
+            		}
+            		itemIndex++;
+            	}
+            	sortedMessages = data3;
+            	// sortedMessages = data4;
+            	cb(null, sortedMessages);	
+            });	//Flags.find end
 
 			// sortedMessages = data3;
 			// cb(null, sortedMessages);
