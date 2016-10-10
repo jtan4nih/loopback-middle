d53c1b43c533c88b0f1b12fa6eedf6a08e9825b6
info <info@sciits.com>
2016-10-10 10:24:00 -0400

    Added cleardb

--- scripts/rimport.bat
+++ scripts/rimport.bat
@@ -1,9 +1,9 @@
-mysql -h 50.28.56.122 -u root -p -e "drop database stem2dev_db"
+mysql -h us-cdbr-iron-east-04.cleardb.net -u bf679ee3067ed5 -p%1 -e "drop database stem2dev_db"
 
-mysql -h 50.28.56.122 -u root -p -e "create database stem2dev_db"
+mysql -h us-cdbr-iron-east-04.cleardb.net -u bf679ee3067ed5 -p%1 -e "create database stem2dev_db"
 
-mysql -h 50.28.56.122 -u root -p -e "set GLOBAL time_zone = '+00:00'"
+mysql -h us-cdbr-iron-east-04.cleardb.net -u bf679ee3067ed5 -p%1 -e "set GLOBAL time_zone = '+00:00'"
 
-mysql -h 50.28.56.122 -u root -p -e "SELECT @@global.time_zone, @@session.time_zone"
+mysql -h us-cdbr-iron-east-04.cleardb.net -u bf679ee3067ed5 -p%1 -e "SELECT @@global.time_zone, @@session.time_zone"
 
-mysql -h 50.28.56.122 -u root -p stem2dev_db < stem2dev_db.sql
+mysql -h us-cdbr-iron-east-04.cleardb.net -u bf679ee3067ed5 -p%1 stem2dev_db < stem2dev_db.sql
