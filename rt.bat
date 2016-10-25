215fd6edb0d2d212395bfa953dbc87457136fa21
info <info@sciits.com>
2016-10-24 22:02:53 -0400

    .

--- rt.bat
+++ rt.bat
@@ -2,8 +2,6 @@
 echo "*********************************************************************************"
 echo "Make sure the APIHOSTs are up and running!" c.f. ionic-mobile/tests/ms and rl
 echo "You should hit Ctrl + C to quit if it is not and start the tests again."
-echo "NOTES: The tests against the real APIHOST is currently broken, thus please wait
-echo "until you see '15 tests, 12 assertions, 2 failures, 0 skipped' before ending the test!
 echo "*********************************************************************************"
 pause
 @echo on
