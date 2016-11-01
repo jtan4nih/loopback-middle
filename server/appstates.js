98a933f7ef5ba11deeb46fd19cb5ff579f9d1492
info <info@sciits.com>
2016-11-01 00:49:38 -0400

    .

--- server/appstates.js
+++ server/appstates.js
@@ -1,58 +1,69 @@
 var app = module.exports = {
 
-    check: function(app, model) {
+    check: function(app, model, next) {
         var Powerups = app.models.Powerups;
         var Quests = app.models.Quests;
+        var Economy = app.models.Economy;
         var Goals = app.models.Goals;
-        var state = {}; //this needs to be from the economy!!!
-        if(model instanceof Powerups) {
-            if(model.name === 'Stock the Fridge') {
-                if(model.completed) state.completedCount++; //logic #1
-                state.points = model.weight * 1 + state.points;     //logic #2
-                if(state.completedCount === state.totalCount) state.progress = true; //advance to the next quest!
-            } else
-            if(model.name === 'Walk it Off') {
-                if(model.completed) state.completedCount++; //logic #1
-                state.points = model.weight * 1 + state.points;     //logic #2
-                if(state.completedCount === state.totalCount) state.progress = true; //advance to the next quest!
-            } else
-            if(model.name === 'Tell a Friend') {
-                if(model.completed) state.completedCount++; //logic #1
-                state.points = model.weight * 2 + state.points;     //logic #2
-                if(state.completedCount === state.totalCount) state.progress = true; //advance to the next quest!
-            }
-        } else
-        if(model instanceof Quests) {
-            var threshold = 1;  //just happened to be the total quests - 1
-            if(model.name === 'Get it Started') {
-                if(model.completed) state.completedCount++; //logic #1
-                state.points = model.weight * 1 + state.points;     //logic #2
-                if(state.completedCount === state.totalCount) {
-                    state.award = 'rockie';
-                    state.awardCount++;
-                }
-                if(state.awardCount > threshold) {
-                    state.progress = true; //advance to the goal
+
+        Economy.findOne({where: {"owner": messageid, "owner": loggedinownerid}}, function (err, data) {
+            var state = evaluate(data);
+            next(state);
+        });
+
+        function evaluate(data) {
+            var state = data; //this needs to be from the economy!!!
+            if(model instanceof Powerups) {
+                if(model.name === 'Stock the Fridge') {
+                    if(model.completed) state.completedCount++; //logic #1
+                    state.points = model.weight * 1 + state.points;     //logic #2
+                    if(state.completedCount === state.totalCount) state.progress = true; //advance to the next quest!
+                } else
+                if(model.name === 'Walk it Off') {
+                    if(model.completed) state.completedCount++; //logic #1
+                    state.points = model.weight * 1 + state.points;     //logic #2
+                    if(state.completedCount === state.totalCount) state.progress = true; //advance to the next quest!
+                } else
+                if(model.name === 'Tell a Friend') {
+                    if(model.completed) state.completedCount++; //logic #1
+                    state.points = model.weight * 2 + state.points;     //logic #2
+                    if(state.completedCount === state.totalCount) state.progress = true; //advance to the next quest!
                 }
             } else
-            if(model.name === 'Walkie Talkie') {
-                if(model.completed) state.completedCount++; //logic #1
-                state.points = model.weight * 1 + state.points;     //logic #2
-                if(state.completedCount === state.totalCount) {
-                    state.award = 'senior';
-                    state.awardCount++;
+            if(model instanceof Quests) {
+                var threshold = 1;  //just happened to be the total quests - 1
+                if(model.name === 'Get it Started') {
+                    if(model.completed) state.completedCount++; //logic #1
+                    state.points = model.weight * 1 + state.points;     //logic #2
+                    if(state.completedCount === state.totalCount) {
+                        state.award = 'rockie';
+                        state.awardCount++;
+                    }
+                    if(state.awardCount > threshold) {
+                        state.progress = true; //advance to the goal
+                    }
+                } else
+                if(model.name === 'Walkie Talkie') {
+                    if(model.completed) state.completedCount++; //logic #1
+                    state.points = model.weight * 1 + state.points;     //logic #2
+                    if(state.completedCount === state.totalCount) {
+                        state.award = 'senior';
+                        state.awardCount++;
+                    }
+                    if(state.awardCount > threshold) {
+                        state.progress = true; //advance to the goal
+                    }
                 }
-                if(state.awardCount > threshold) {
-                    state.progress = true; //advance to the goal
+            } else
+            if(model instanceof Goals) {
+                var threshold = 1;  //only a single goal for now
+                if(model.name === 'Yeah!') {
+                    //reset the states and start all over
+                    state = {};
                 }
             }
-        } else
-        if(model instanceof Goals) {
-            var threshold = 1;  //only a single goal for now
-            if(model.name === 'Yeah!') {
-                //reset the states and start all over
-                state = {};
-            }
+
+            return state;
         }
     }
 
