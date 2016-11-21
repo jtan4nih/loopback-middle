383e7d365513674b75d0a88c99254333aeb44836
Your Name <you@example.com>
2016-11-21 15:36:37 +0800

    Changed on new machine

--- i.bat
+++ i.bat
@@ -1,3 +1,1 @@
-pause "----> PLEASE RUN THIS IN A NON-POWERSHELL WINDOW e.g. "Run as administrator"!!! Hit any key if you have, otherwise hit Ctrl + C to break..."
-
-npm i
+nodist use 6.6.0 && npm i
--- ig.bat
+++ ig.bat
@@ -1,5 +1,4 @@
-echo LAUNCH THIS IN THE ADMINISTRATOR WINDOW!
-pause
+pause "----> PLEASE RUN THIS IN A NON-POWERSHELL WINDOW e.g. "Run as administrator"!!! Hit any key if you have, otherwise hit Ctrl + C to break..."
 
 :nodist use stable && npm install -g apiconnect && npm install -g jasmine-node-debug
-nodist use stable && npm install -g strongloop && npm install -g jasmine-node-debug
+choco install dotnet3.5 && cinst VisualStudio2012Professional && nodist use 6.6.0 && npm install -g strongloop && npm install -g jasmine-node-debug
