9c1928bbaad0ee4cdd82b45cd67734cf0ce64934
info <info@sciits.com>
2016-12-20 11:48:11 +0800

    .

--- ig.bat
+++ ig.bat
@@ -1,4 +1,6 @@
 pause "----> PLEASE RUN THIS IN A NON-POWERSHELL WINDOW e.g. "Run as administrator"!!! Hit any key if you have, otherwise hit Ctrl + C to break..."
 
 :nodist use stable && npm install -g apiconnect && npm install -g jasmine-node-debug
-choco install dotnet3.5 && cinst VisualStudio2012Professional && nodist use 6.6.0 && npm install -g strongloop && npm install -g jasmine-node-debug
+:choco install dotnet3.5 && cinst VisualStudio2012Professional && nodist use 6.6.0 && npm install -g strongloop && npm install -g jasmine-node-debug
+
+npm install -g apiconnect
--- ra.bat
+++ ra.bat
@@ -1,1 +1,3 @@
-slc arc
+:slc arc
+set SKIP_LOGIN=true&& apic edit
+:set PORT=9090&& set HOST=localhost&& apic edit
